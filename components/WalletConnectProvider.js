b5844ca934dc560aafd1864fdbb2566786e9c67a
Benjamin | benjamintan.eth <124853084+0xBenjamintan@users.noreply.github.com>
2024-04-05 02:30:20 +0800

    new rpc

--- components/WalletConnectProvider.js
+++ components/WalletConnectProvider.js
@@ -14,7 +14,7 @@ export const WalletConnectProvider = ({ children }) => {
 
   const endpoint = useMemo(() => {
     if (network === WalletAdapterNetwork.Devnet) {
-      return "https://fragrant-wider-dawn.solana-devnet.discover.quiknode.pro/e28bd823c1b8e232f77b0d36425d13b00e22c4f9/";
+      return "https://devnet.helius-rpc.com/?api-key=226d5c15-0a78-4b72-b742-ed0b5dc08b97";
     }
 
     return clusterApiUrl(network);
